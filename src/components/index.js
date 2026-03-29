1ab1e20a3f81bb64fdd3637c6e7701f7c7d4f0db
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2026-03-29 20:12:47 +0200

    Change logo image format from JPG to PNG

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export {default as Logo} from '../img/sherifemad.jpg'
+export {default as Logo} from '../img/sherifemad.png'
 export {default as Link} from 'next/link'
 export {default as Footer} from './Footer'
 export {default as Line} from './UI/Line'
