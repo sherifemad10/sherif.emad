0d51344dd4799156748381f5be7c00a73ac3cc06
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2026-03-29 20:09:48 +0200

    Fix logo image import path in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export {default as Logo} from '../img/sherifemadd.jpg'
+export {default as Logo} from '../img/sherifemad.jpg'
 export {default as Link} from 'next/link'
 export {default as Footer} from './Footer'
 export {default as Line} from './UI/Line'
